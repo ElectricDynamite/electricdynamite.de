c046ae28f711bc1ca6904fe51b6e7c7fecfabf5a
Philipp Geschke <pg@electricdynamite.de>
2012-04-11 12:51:42 +0200

    Rewriterules, german privacy policy, etc

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,13 +1,21 @@
 <?php
+$mNext = "";
+$mDeCountries = array('de', 'at', 'ch');
+$mURLs = array('de' => '/de/', 'en' => '/en/');
+
+error_log(substr($_GET["next"],1,2));
+
+if(isset($_GET["next"]) && !array_key_exists(substr($_GET["next"],1,2), $mURLs)) {
+  $mNext = $_GET["next"];
+}
 $mLocation = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
 
-$mDeCountries = new Array('de', 'at', 'ch');
-$mURLs = new Array('de' => '/de/', 'en' => '/en/');
+$mLoc = strtolower($mLocation["geoplugin_countryCode"]);
 
-if(in_array($mLocation["geoplugin_countryCode"], $mDeCountries)) {
-  header( 'Location: '+$mURLs['de'] );
+if(in_array( $mLoc, $mDeCountries)) {
+  header( 'Location: '.$mURLs['de'].$mNext );
 } else {
-  header( 'Location: '+$mURLs['en'] );
+  header( 'Location: '.$mURLs['en'].$mNext );
 }
 
 ?>
